7e56e5cc3baea8c8fcc34a35b3454eebe18adcbe
yminjuu <ymj0580@naver.com>
2025-08-02 16:10:55 +0900

    13975 풀이

--- week5/MINJU/2212.cpp
+++ week5/MINJU/2212.cpp
@@ -5,8 +5,9 @@
 
 using namespace std;
 int tmp, posCnt=0, ans=0;
-int N; 
-int K; 
+int N; // 센서 개수
+int K; // 최대 세울 수 있는 집중국 개수
+// (시그마) 집중국의 수신가능영역의 최소를 구해야 됨
 vector<int> vec;
 vector<int> space;
 stack<int> stck;
@@ -23,16 +24,17 @@ int main()
       vec.push_back(tmp);
    }
 
-   sort(vec.begin(), vec.end());
+   sort(vec.begin(), vec.end()); // 오름차순 정렬
 
+   // 바로 센서 위치마다 기지국 세울 수 있음
    if (N<=K) {
       cout << 0;
       return 0;
    }
 
    for (int i=1; i<N; i++) space.push_back(vec[i]-vec[i-1]);
-   sort(space.begin(), space.end(), greater<int>()); 
-   for (int i=0; i<N-1; i++) {stck.push(space[i]); }
+   sort(space.begin(), space.end(), greater<int>()); // 내림차순
+   for (int i=0; i<N-1; i++) {stck.push(space[i]); }// 차가 작은 게 맨 위에
 
    while (stck.size()>K-1){
       ans+=stck.top();
